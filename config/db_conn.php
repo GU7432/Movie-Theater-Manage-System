35d093c66517ae8c862e884455893fc9940cfba3
HankWang <xihongw103@gmail.com>
2025-12-07 16:05:38 +0800

    test database connection

---
+++ config/db_conn.php
@@ -0,0 +1,13 @@
+<?php
+    $user = 'root';
+    $password = '2Gguurou';
+    try{
+        $db = new 
+        PDO('mysql: host= localhost ; dbname=homework;charset=utf8' , $user,$password);
+        $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+        $db -> setAttribute(PDO::ATTR_EMULATE_PREPARES,false);   
+    }catch(PDOException $e){
+        Print "ERROR!: " . $e ->getMessage();
+        die(); 
+    }
+?>
